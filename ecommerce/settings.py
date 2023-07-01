515ada6f67105e91dc46a661c57aa9c91eb477e0
safarow <asifseferov00@gmail.com>
2023-07-01 15:32:00 +0400

    last

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,18 +46,24 @@ INSTALLED_APPS = [
     'customer',
     'payment',
     'django_cleanup.apps.CleanupConfig',
+    'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "django.middleware.locale.LocaleMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'ecommerce.urls'
 
--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'shop'
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.product_list, name='product-list'),
     path('products/<int:pk>/<str:slug>/', views.product_detail, name='product-detail'),
-    path('review/<int:pk>/', views.review, name='review')
+    path('review/<int:pk>/', views.review, name='review'),
+    path('api/', include('shop.api.urls'))
 ]
